72c4b32bfb7b48ce4ab6ce959588d36c48dd1bfd
Joshua Mun <joshmun23@gmail.com>
2015-08-06 21:48:12 -0400

    get static high charts available, need to incorporate ajax jquery next

--- app/assets/javascripts/daily_chart.js
+++ app/assets/javascripts/daily_chart.js
@@ -1,47 +1,26 @@
-/*!
- * Chart.js
- * http://chartjs.org/
- * Version: 1.0.2
- *
- * Copyright 2015 Nick Downie
- * Released under the MIT license
- * https://github.com/nnnick/Chart.js/blob/master/LICENSE.md
- */
+$(function () {
+  $('#charts-container').highcharts({
+    var data = $.ajax('/users/' + userID + '/smokes', function() {
 
+    });
 
-// Load the Visualization API and the piechart package.
-$('#chart-button').on('click', function(e) {
-  e.preventDefault();
-  debugger
-
-  google.load('visualization', '1.0', {'packages':['corechart']});
-
-  // Set a callback to run when the Google Visualization API is loaded.
-  google.setOnLoadCallback(drawChart);
-
-  // Callback that creates and populates a data table,
-  // instantiates the pie chart, passes in the data and
-  // draws it.
-  function drawChart() {
-
-    // Create the data table.
-  var data = google.visualization.arrayToDataTable([
-     ['Day', 'Total Cigarettes', 'Cost', { role: 'style' }],
-     ['7/1/2015', 10, 8.94, '#b87333'],            // RGB value
-     ['7/2/2015', 14, 10.49, 'silver'],            // English color name
-     ['7/3/2015', 18, 19.30, 'gold'],
-
-   ['7/4/2015', 20, 21.45, 'color: #e5e4e2' ], // CSS-style declaration
-  ]);
-
-    // Set chart options
-    var options = {'title':'Daily Cigarette Consumption',
-                   'width':500,
-                   'height':400,
-                 };
-
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.ColumnChart(document.getElementById('charts'));
-    chart.draw(data, options);
-  }
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Cigarette Consumption'
+    },
+    xAxis: {
+        categories: ['Day 1', 'Day 2', 'Day 3']
+    },
+    yAxis: {
+        title: {
+            text: 'Cigarettes Smoked'
+        }
+    },
+    series: [{
+        name: 'Jane',
+        data: [1, 0, 4]
+    }]
+  });
 });
